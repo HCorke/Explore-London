02fc42e2ae42080d2e4e89128f389beffacb1685
HCorke <hannah.corke@sainsburys.co.uk>
2023-02-02 11:29:19 +0000

    boroughs in correct areas

--- frontend/src/Components/Borough.js
+++ frontend/src/Components/Borough.js
@@ -6,27 +6,9 @@ export const Borough = ({ borough }) => {
   const [isOpenBoroughModal, setIsOpenBoroughModal] = useState(false);
 
   return borough.boroughName ? (
-    <div id="areas">
-      <div className="Areas">
-        <div className="North">
-          <h1>North London</h1>
-        </div>
-        <div className="East">
-          <h1>East London</h1>
-        </div>
-        <div className="South">
-          <h1>South London</h1>
-        </div>
-        <div className="West">
-          <h1>West London</h1>
-        </div>
-        <div className="Central">
-          <h1>Central London</h1>
-        </div>
-      </div>
-      <div>
-        <div id="borough-name">{borough.boroughName}</div>
-      </div>
+    <div>
+      <div id="borough-name">{borough.boroughName}</div>
+
       {isOpenBoroughModal && (
         <BoroughModal
           borough={borough}
--- frontend/src/Components/BoroughsPage.js
+++ frontend/src/Components/BoroughsPage.js
@@ -6,12 +6,80 @@ export const BoroughsPage = ({ boroughs, setBoroughs }) => {
 
   return boroughs[0] ? (
     <div className="main">
-    <div id="boroughs">
-      <div id="boroughs-grid">   
-          <div id="borough">
-            {boroughs.map((borough) => (
-              <Borough borough={borough} key={borough.boroughId} />
-            ))}
+      <div className="Areas">
+        <div className="North">
+          <h1>North London</h1>
+          <div id="north-boroughs">
+            {boroughs
+              .filter((borough) => {
+                if (borough.area === "North London") {
+                  return borough;
+                }
+                return null;
+              })
+              .map((borough) => (
+                <Borough borough={borough} key={borough.boroughId} />
+              ))}
+          </div>
+        </div>
+        <div className="East">
+          <h1>East London</h1>
+          <div id="east-boroughs">
+            {boroughs
+              .filter((borough) => {
+                if (borough.area === "East London") {
+                  return borough;
+                }
+                return null;
+              })
+              .map((borough) => (
+                <Borough borough={borough} key={borough.boroughId} />
+              ))}
+          </div>
+        </div>
+        <div className="South">
+          <h1>South London</h1>
+          <div id="south-boroughs">
+            {boroughs
+              .filter((borough) => {
+                if (borough.area === "South London") {
+                  return borough;
+                }
+                return null;
+              })
+              .map((borough) => (
+                <Borough borough={borough} key={borough.boroughId} />
+              ))}
+          </div>
+        </div>
+        <div className="West">
+          <h1>West London</h1>
+          <div id="west-boroughs">
+            {boroughs
+              .filter((borough) => {
+                if (borough.area === "West London") {
+                  return borough;
+                }
+                return null;
+              })
+              .map((borough) => (
+                <Borough borough={borough} key={borough.boroughId} />
+              ))}
+          </div>
+        </div>
+        <div className="Central">
+          <h1>Central London</h1>
+          <div id="central-boroughs">
+            {boroughs
+              .filter((borough) => {
+                if (borough.area === "Central London") {
+                  return borough;
+                }
+                return null;
+              })
+              .map((borough) => (
+                <Borough borough={borough} key={borough.boroughId} />
+              ))}
           </div>
         </div>
       </div>
